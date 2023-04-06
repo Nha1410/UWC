a3e1064efda76f906b9d94e228b3f1ba061cee45
vuong2407 <vanvuong24072001@gmail.com>
2023-04-06 23:53:43 +0700

    add fontawesome and Tailwind CSS

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,5 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
+
 function Home() {
-    return <h2>Home Page</h2>;
+    return (
+        <div>
+            <h2 className="text-3xl font-bold underline">Home Page</h2>;
+            <FontAwesomeIcon icon={icon({ name: 'user-secret' })} />
+        </div>
+    );
 }
 
 export default Home;
