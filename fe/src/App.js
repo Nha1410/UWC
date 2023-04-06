81d84aae023103097733b417678da4302cbb7311
vuong2407 <vanvuong24072001@gmail.com>
2023-04-06 23:42:45 +0700

    config router and add master layout

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,8 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import DefaultLayout from './components/Layout/DefaultLayout';
+
 function App() {
     return (
-        <div className="App">
-            <h1>UWC</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <DefaultLayout>
+                                <Home />
+                            </DefaultLayout>
+                        }
+                    />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="signup" element={<SignUp />} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
