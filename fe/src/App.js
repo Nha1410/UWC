a528946392018f507ed286cdba4bb49bdd46fa00
ienaga <ienaga@hab-co.jp>
2023-04-08 22:08:12 +0700

    add_UI_for_management

--- fe/src/App.js
+++ fe/src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
-import Management from './pages/Management';
+import TaskManagement from './pages/TaskManagement';
+import StaffManagement from './pages/StaffManagement';
 import DefaultLayout from './components/Layout/DefaultLayout';
 import AuthLayout from './components/Layout/AuthLayout';
 import SidebarLogin from './components/Layout/components/SidebarAuth/Login';
 import SidebarSignUp from './components/Layout/components/SidebarAuth/Signup';
+import VehicalManagement from './pages/VehicleManagement';
 
 function App() {
     return (
@@ -41,7 +43,23 @@ function App() {
                         path="/task-management"
                         element={
                             <DefaultLayout>
-                                <Management />
+                                <TaskManagement />
+                            </DefaultLayout>
+                        }
+                    />
+                    <Route
+                        path="/staff-management"
+                        element={
+                            <DefaultLayout>
+                                <StaffManagement />
+                            </DefaultLayout>
+                        }
+                    />
+                    <Route
+                        path="/vehicle-management"
+                        element={
+                            <DefaultLayout>
+                                <VehicalManagement />
                             </DefaultLayout>
                         }
                     />
