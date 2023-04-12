6e6499bf410ecd471897c16061a2f55d0bc48a18
vuong2407 <vanvuong24072001@gmail.com>
2023-04-12 21:43:34 +0700

    fix title box item dashboard

--- fe/src/components/Commons/DashboardBoxContent/index.js
+++ fe/src/components/Commons/DashboardBoxContent/index.js
@@ -5,7 +5,7 @@ function BoxContent({ title, children }) {
     return (
         <div className="flex flex-col h-full min-w-fit overflow-auto justify-top p-[10px] bg-[#364153] rounded-lg">
             <div className="flex justify-between text-white">
-                <span className="font-bold">{title}</span>
+                <span className="font-bold pt-[10px] pb-[20px]">{title}</span>
                 <FontAwesomeIcon icon={faEllipsis} className="text-white pl-[10px]" />
             </div>
             <div className="flex-1 min-h-0 overflow-auto">{children}</div>
