b48505509bb0f5397ed0dcf93040dc9df7360298
vuong2407 <vanvuong24072001@gmail.com>
2023-04-14 00:16:24 +0700

    add ORM Sequelize with MySQL to interact with database

--- be/index.js
+++ be/index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const route = require("./routes");
-const db = require("./app/models/index");
+const db = require("./models");
 
 const app = express();
 const port = process.env.PORT;
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 try {
-  db.sequelize.sync();
+  db.sequelize.authenticate();
   console.log("connect MySQL success");
 } catch (error) {
   console.log(error);
